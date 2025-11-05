d29299252d3a6f491fc85c772a6b21440043410e
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2025-11-05 16:41:36 -0300

    Refatoração do escopo de User

--- Gerenciador-Projetos/src/main/java/br/com/ufrn/imd/Project_Manager/repository/UserRepository.java
+++ Gerenciador-Projetos/src/main/java/br/com/ufrn/imd/Project_Manager/repository/UserRepository.java
@@ -1,17 +1,28 @@
 package br.com.ufrn.imd.Project_Manager.repository;
 
 import br.com.ufrn.imd.Project_Manager.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User>{
+public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmailIgnoreCase(String email);
     boolean existsByNameIgnoreCase(String name);
 
     Optional<User> findByEmailIgnoreCase(String email);
     Optional<User> findByNameIgnoreCase(String name);
+
+    @Query("""
+    SELECT u FROM User u
+        WHERE (:name IS NULL OR LOWER(u.name) LIKE LOWER(CONCAT('%', :name, '%')))
+        AND (:position IS NULL OR LOWER(u.position) LIKE LOWER(CONCAT('%', :position, '%')))
+    """)
+    Page<User> searchUsers(@Param("name") String name,
+                           @Param("position") String position,
+                           Pageable pageable);
 }
