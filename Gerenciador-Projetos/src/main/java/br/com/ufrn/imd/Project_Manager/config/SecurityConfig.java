4570debcee0e59d8fbfdee10324cfec96ed38cae
Danilo25 <danilo25aciole@gmail.com>
2025-12-08 15:11:23 -0300

    add-clients-page-merger

--- Gerenciador-Projetos/src/main/java/br/com/ufrn/imd/Project_Manager/config/SecurityConfig.java
+++ Gerenciador-Projetos/src/main/java/br/com/ufrn/imd/Project_Manager/config/SecurityConfig.java
@@ -42,19 +42,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder() {
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                System.out.println("--- [DEBUG AUTH] Iniciando Comparação de Senhas ---");
-                System.out.println("Senha digitada (raw): " + rawPassword);
-                System.out.println("Hash armazenado no banco: " + encodedPassword);
-                
-                boolean matches = super.matches(rawPassword, encodedPassword);
-                
-                System.out.println("Resultado da comparação: " + (matches ? "SUCESSO (Iguais)" : "FALHA (Diferentes)"));
-                System.out.println("---------------------------------------------------");
-                return matches;
-            }
-        };
+        return new BCryptPasswordEncoder();
     }
 }
